39fabe12e4d0756458a88ca30ff33aa8def17be7
jiles.ru@gmail.com <jiles.ru@gmail.com@a2046d06-64f2-91c0-55e4-cc4e745ffb5b>
2012-10-10 04:20:50 +0000

    Creating brunch with my code only

---
+++ branches/2012-10-10_rev4/src/com/insolence/admclient/SendCommandAsyncTask.java
@@ -0,0 +1,54 @@
+package com.insolence.admclient;
+
+import android.app.ListActivity;
+import android.os.AsyncTask;
+import android.view.View;
+import android.widget.ListView;
+
+public class SendCommandAsyncTask  extends AsyncTask<Void, Void, Void>{
+
+	protected ListActivity _listActivity;
+	private String _command;
+	private String _id;
+	
+	public SendCommandAsyncTask(ListActivity listActivity, String command){
+		_listActivity = listActivity;
+		_command = command;
+		_id = null;
+	}
+	
+	public SendCommandAsyncTask(ListActivity listActivity, String command, String id){
+		_listActivity = listActivity;
+		_command = command;
+		_id = id;
+	}
+	
+
+	@Override
+	protected Void doInBackground(Void... arg0) {
+		if (_id == null)
+			DownloadItemListManager.getInstance().SendGroupCommand(_command);
+		else
+			DownloadItemListManager.getInstance().SendCommand(_command, _id);
+		DownloadItemListManager.getInstance().getDownloadItems(true);
+		return null;
+	}
+	
+	@Override
+	protected void onPostExecute(Void result){		
+		DownloadItemListAdapter adapter = new DownloadItemListAdapter(_listActivity);
+		
+		ListView list = _listActivity.getListView();
+		int savedPosition = list.getFirstVisiblePosition();
+	    View firstVisibleView = list.getChildAt(0);
+	    int savedListTop = (firstVisibleView == null) ? 0 : firstVisibleView.getTop();	
+		
+		_listActivity.setListAdapter(adapter);
+		
+		if (savedPosition >= 0) { //initialized to -1
+		      list.setSelectionFromTop(savedPosition, savedListTop);
+		    }
+
+		
+	}
+}
