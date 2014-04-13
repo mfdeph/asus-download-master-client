44e1091a06c4e7cd9fac6d306ac3b54668dab6a4
jiles.ru@gmail.com <jiles.ru@gmail.com@a2046d06-64f2-91c0-55e4-cc4e745ffb5b>
2014-04-13 19:10:46 +0000

    Multiple fils part 1

---
+++ branches/jiles.ru_workspace_1/src/com/insolence/admclient/entity/DownloadInfo.java
@@ -0,0 +1,51 @@
+package com.insolence.admclient.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DownloadInfo {
+	
+	String torrentName;
+	List<DownloadFileInfo> files;
+	
+	public DownloadInfo(String torrentName) {
+		super();
+		this.torrentName = torrentName;
+		files = new ArrayList<DownloadFileInfo>();
+	}
+
+	public String getTorrentName() {
+		return torrentName;
+	}
+
+	public List<DownloadFileInfo> getFiles() {
+		return files;
+	}
+	
+	public static DownloadInfo parse(String response){
+		try{
+			String scriptParameter = response.split("\"")[1];
+			String[] parts = scriptParameter.split(",\\s#");
+			String torrentName = parts[1].trim().substring(1);
+			DownloadInfo result = new DownloadInfo(torrentName);
+			for (int i = 2; i < parts.length; i++){
+				String[] subParts = parts[i].split("#");
+				int length = subParts.length;
+				String id = subParts[0];
+				String fileSize = subParts[length - 2];
+				String fileName = subParts[length - 1];
+				for(int j = 1; j < length - 2; j++){
+					if (!subParts[j].equalsIgnoreCase("") && !subParts[j].equalsIgnoreCase("none")){
+						fileName = fileName + "/" + subParts[j].trim();
+					}
+				}
+				
+				result.getFiles().add(new DownloadFileInfo(id, fileName, fileSize));
+			}
+			return result;
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
